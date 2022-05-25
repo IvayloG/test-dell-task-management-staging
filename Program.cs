e338337ae571de081c27222adeedb2dc3af3dfc4
IvayloG <inj.iganchev@gmail.com>
2022-05-25 16:03:44 +0300

    Task management initial version

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using IgniteUI.Blazor.Controls;
+using TaskManagement;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+RegisterIgniteUI(builder.Services);
+
+await builder.Build().RunAsync();
+
+void RegisterIgniteUI(IServiceCollection services)
+{
+    services.AddIgniteUIBlazor(
+        typeof(IgbNavbarModule),
+        typeof(IgbButtonModule),
+        typeof(IgbIconModule),
+        typeof(IgbRippleModule),
+        typeof(IgbAvatarModule),
+        typeof(IgbDropdownModule),
+        typeof(IgbDropdownHeaderModule),
+        typeof(IgbDropdownItemModule),
+        typeof(IgbCardModule),
+        typeof(IgbListModule)
+    );
+}
